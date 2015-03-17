6b86c46489bddd71d94987656936bf1732d985b3
Krishna Kalubandi <krishnakalubandi@gmail.com>
2015-03-17 19:51:54 +0530

    test

---
+++ app/src/main/java/com/ieeecs/android/TeamFragment.java
@@ -0,0 +1,17 @@
+package com.ieeecs.android;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Krishna Kalubandi on 17-03-2015.
+ */
+public class TeamFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
